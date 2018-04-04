f5fcc0c0c99ebe1a140e0cb6b42c96a05eb72734
Michael Saunders <michaelsaunders@outlook.com>
2018-04-03 22:15:58 -0700

    Initial commit

---
+++ Office_initialize.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../Office.Runtime.js" />
+/// <reference path="../Excel.js" />
+/// <reference path="./Udf_definitions.js" />
+/// <reference path="./Udf.js" />
+
+(function () {
+	"use strict";
+
+	Office.initialize = function (reason) {
+		return Excel.CustomFunctions.initialize();
+	};
+})();
---
+++ Udf.js
@@ -0,0 +1,39 @@
+﻿function add10(num) {
+	return num + 10;
+}
+
+function add20(num) {
+	return num + 20;
+}
+
+function add30(num) {
+	return new OfficeExtension.Promise(function(resolve) {
+		setTimeout(function() {
+			resolve(num + 30);
+		}, 1);
+	});
+}
+
+function isEven(num) {
+	return num % 2 == 0;
+}
+
+function getDay() {
+	var d = new Date();
+	var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	return days[d.getDay()];
+}
+
+function getDummyString(len) {
+	var base = "123456789 ";
+
+	var div = Math.floor(len / 10);
+	var rem = len - (div * 10);
+
+	var result = "";
+	for (var i = 0; i < div; i++) {
+		result += base;
+	}
+	result += base.substr(0, rem);
+	return result;
+}
